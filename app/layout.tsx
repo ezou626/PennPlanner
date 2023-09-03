a65ed9566ed2590e73b669482e94f34249587483
Eric Zou <ezou626@gmail.com>
2023-09-02 23:32:22 -0400

    add plan page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Roboto } from 'next/font/google';
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+const roboto300 = Roboto( {weight: "300"} );
+
+export default function RootLayout( { children }: { children: React.ReactNode } ) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={roboto300.className}>
+      <body>
+        {children}
+      </body>
     </html>
   )
 }
