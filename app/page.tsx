7f020fa96dee7f6f5eb7e2d0cef2f3eaef5b1205
Eric Zou <ezou626@gmail.com>
2023-09-05 12:27:26 -0400

    home page final touches

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Metadata } from 'next';
+import Credit from '../components/credit';
+import Login from '../components/login'
 
 export const metadata: Metadata = {
   title: 'Penn Planner',
@@ -13,9 +15,13 @@ export default function HomePage({
   searchParams: { [key: string]: string | string[] | undefined }
 }) {
   return (
-    <main className='px-10 overflow-y-clip space-y-5 py-32 mx-auto max-w-5xl'>
-      <h1 className='text-6xl font-bold '><span className='text-sky-950'>Penn</span> <span className='text-red-900'>Planner</span></h1>
-      <h2 className='text-4xl'>Academic Plan Assistant for Penn Students</h2>
-    </main>
+    <>
+      <Login />
+      <main className='px-10 overflow-y-clip space-y-5 py-32 mx-auto max-w-5xl'>
+        <h1 className='text-6xl font-bold '><span className='text-sky-950'>Penn</span> <span className='text-red-900'>Planner</span></h1>
+        <h2 className='text-4xl'>An (Unofficial) Academic Planner for Penn Students</h2>
+      </main>
+      <Credit />
+    </>
   )
 }
