29f3977f04dfe4dd5c9bb6823c588e21d8a3b0ec
Eric Zou <ezou626@gmail.com>
2023-09-08 05:31:39 -0400

    remove console log and add sign out button

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -3,11 +3,10 @@
 Navigation bar for website
 */
 import Link from 'next/link';
-import { useSession } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
 
 export default function NavigationBar() {
 	const {data: session} = useSession();
-  console.log(session);
 	if (session != null) {
     const name: string = session.user!.name!;
 		return (
@@ -16,6 +15,7 @@ export default function NavigationBar() {
 				<ul className='float-right space-x-10 flex'>
 					<li><Link href = '/'>Home</Link></li>
 					<li><Link href = 'plan'>Plan</Link></li>
+					<li><button onClick = {() => signOut({callbackUrl: '/'})}>Sign Out</button></li>
 				</ul>
 			</nav>
 		)
