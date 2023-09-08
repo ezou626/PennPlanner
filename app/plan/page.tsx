ff35bd3f5d0a9a1350d71922dc3fc2339ba65d68
Eric Zou <ezou626@gmail.com>
2023-09-08 05:25:19 -0400

    remove login page from app folder

--- app/plan/page.tsx
+++ app/plan/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Metadata } from 'next';
 import Credit from '../../components/credit';
-import Login from '../../components/navigation';
+import NavigationBar from '../../components/navigation';
 import Dashboard from '../../components/dashboard';
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "../api/auth/[...nextauth]/route";
@@ -20,7 +20,7 @@ export default async function PlanPage({
   const session = await getServerSession(authOptions);
   return (
     <>
-      <Login />
+      <NavigationBar />
       <main className='px-10 overflow-y-clip py-10'>
         <Dashboard></Dashboard>
       </main>
