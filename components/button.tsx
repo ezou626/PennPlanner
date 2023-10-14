ff1f5511eb4dc0ebfa18dd9d2145eb2b78319f77
Eric Zou <ezou626@gmail.com>
2023-10-14 10:31:14 -0400

    fix tab spacing

--- components/button.tsx
+++ components/button.tsx
@@ -2,15 +2,17 @@
 import React from "react";
 
 type ButtonProps = {
-    color: string;
-    onClick: () => void;
-    children: React.ReactNode;
+  color: string;
+  onClick: () => void;
+  children: React.ReactNode;
 };
 
 const Button: React.FunctionComponent<ButtonProps> = ({ color, onClick, children }) => {
-    let settings: string = 'text-md rounded px-2 py-2 text-white font-bold ';
-    settings += `bg-${color}-500 hover:bg-${color}-700`
-    return <button className={settings} onClick={onClick}>{children}</button>
+  let settings: string = 'text-md rounded px-2 py-2 text-white font-bold ';
+  settings += `bg-${color}-500 hover:bg-${color}-700`;
+  return <button className={settings} onClick={onClick}>
+    {children}
+    </button>;
 }
 
 export default Button;
