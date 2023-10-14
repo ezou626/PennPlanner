fa09cd6c1faf7c90dca6b83cc752a24d61f3bf44
Eric Zou <ezou626@gmail.com>
2023-10-14 19:03:57 -0400

    finalize login page view

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,22 +1,22 @@
-"use client";
+'use client';
 import React from 'react';
-import NavigationBar from "../../components/navigation";
-import Credit from "../../components/credit";
-import Button from "../../components/button";
+import NavigationBar from '../../components/navigation';
+import Credit from '../../components/credit';
+import Link from 'next/link';
 
 export default function LoginPage() {
   return (<>
     <NavigationBar />
-      <main className='px-10 overflow-y-clip space-y-5 py-32 mx-auto max-w-5xl'>
-        <section className="login text-center block">
-          <div className='text-center block'>
-            <h1 className='text-4xl p-10'>Sign In</h1>
-            <Button color='green' onClick={() => {}}>
-                Sign In
-            </Button>
-            <Button color='blue' onClick={() => {}}>
-                Create an Account
-            </Button>
+      <main className='px-10 overflow-y-clip space-y-5 py-32 mx-auto max-w-5xl center'>
+        <section className='text-center flex place-content-center'>
+          <div className='border border-black rounded px-10 py-5 max-w-2xl'>
+            <h1 className='text-4xl p-5'>Sign In</h1>
+            <form className='space-y-2 py-4'>
+              <input className='focus:outline-none focus:ring-0 border border-black p-2 rounded' type='text' id='username' name='username' placeholder='Username' /><br />
+              <input className='focus:outline-none focus:ring-0 border border-black p-2 rounded' type='password' id='password' name='password' placeholder='Password' /><br />
+              <input className='text-md rounded p-2 w-full text-white font-bold bg-blue-500 hover:bg-blue-700' type='submit' value="Log In"/>
+            </form>
+            <Link className='text-md p-2' href='createAccount'>Create an Account</Link>
           </div>
         </section>
       </main>
