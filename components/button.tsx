cbf7f8af0614694d83ce8a4cf6fa29f57fe1484e
Eric Zou <ezou626@gmail.com>
2023-10-14 10:15:17 -0400

    reduce button complexity again

--- components/button.tsx
+++ components/button.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import React from "react";
 
 type ButtonProps = {
@@ -10,16 +9,7 @@ type ButtonProps = {
 
 const Button: React.FunctionComponent<ButtonProps> = ({ color, onClick, children }) => {
     let settings: string = 'text-md rounded px-2 py-2 text-white font-bold ';
-    switch (color) {
-        case 'green':
-            settings += 'bg-green-500 hover:bg-green-700';
-            break;
-        case 'red':
-            settings += 'bg-red-500 hover:bg-red-700';
-            break;
-        default:
-            settings += 'bg-blue-500 hover:bg-blue-700';
-    }
+    settings += `bg-${color}-500 hover:bg-${color}-700`
     return <button className={settings} onClick={onClick}>{children}</button>
 }
 
